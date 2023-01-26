49c4baa551031f4222e10632cf830ebf871170e3
DavidP1996erea <david.perea@iesnervion.es>
2023-01-26 14:38:32 +0100

    git hub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablaPersonasComponent } from './componentes/tabla-personas/tabla-personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaPersonasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,TablaPersonasComponent]
 })
 export class AppModule { }
